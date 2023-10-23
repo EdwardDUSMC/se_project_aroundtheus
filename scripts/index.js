ed9fa35f019c1030c5aca10a087527915c2fc04f
edwardD <edward.dobrovolny@gmail.com>
2023-10-22 20:46:59 -0500

    stage 1 of project 4

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    tile: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    tile: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    tile: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    tile: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    tile: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    tile: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+console.log(initialCards);
--- vender/scripts/index.js
+++
@@ -1,9 +0,0 @@
-const initialCards =
-  ("object 1",
-  "object 2",
-  "object 3",
-  "object 3",
-  "object 4",
-  "object 5",
-  "object 6");
-console.log(initialCards);
